88c392068ca64c2032732cc8353bccd822bcc9b0
Bartosz Grzebyk <160745@o365.prz.edu.pl>
2023-09-15 14:25:04 +0200

    Added healthchecks

--- dotnet.api/Program.cs
+++ dotnet.api/Program.cs
@@ -1,4 +1,10 @@
+using System.Net.Mime;
+using System.Text.Json;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var postgresConnectionString = builder.Configuration.GetConnectionString("PostgresDatabase");
 
 // Add services to the container.
 
@@ -7,7 +13,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Configuration.AddEnvironmentVariables();
+builder.Services.AddHealthChecks().AddNpgSql(postgresConnectionString, name: "postgres");
 
 var app = builder.Build();
 
@@ -23,5 +29,28 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHealthChecks("/health/ready", new HealthCheckOptions {
+    Predicate = check => check.Name.Equals("postgres"),
+    ResponseWriter = async (context, report) => {
+        var result = JsonSerializer.Serialize(
+            new {
+                status = report.Status.ToString(),
+                checks = report.Entries.Select(entry => new {
+                    name = entry.Key,
+                    status = entry.Value.Status.ToString(),
+                    exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                    duration = entry.Value.Duration.ToString()
+                })
+            }
+        );
+        context.Response.ContentType = MediaTypeNames.Application.Json;
+        await context.Response.WriteAsync(result);
+    }
+});
+
+app.MapHealthChecks("/health/live", new HealthCheckOptions{
+    Predicate = _ => false
+});
 
 app.Run();
